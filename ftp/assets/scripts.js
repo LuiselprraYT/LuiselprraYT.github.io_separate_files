54160d722a8c32628a7c60fc1079154a37622716
LuiselprraYT <71048615+LuiselprraYT@users.noreply.github.com>
2021-08-08 23:55:05 -0700

    added new project FTP

---
+++ ftp/assets/scripts.js
@@ -0,0 +1,44 @@
+function testPwd() {
+    var pwd = document.getElementById('pwd').value;
+
+    if (pwd === 'hrllo') {
+        alert('Nice!')
+        alert('Now...')
+        window.location.href = 'assets/die.gif';
+    } else {
+        alert('Wrong answer');
+        alert('Now I redirect you to a another page');
+        alert('Wait a moment...');
+        alert('Done!');
+        window.location.href = 'assets/wrongpage.html';
+    };
+}
+
+function mobBum(event) {
+    var code = event.keyCode;
+
+    if (code === 8) {
+        var cipeBum = document.getElementById('moob');
+        var sound = cipeBum.cloneNode();
+
+        sound.play();
+    };
+}
+
+function fardPlay() {
+    var epicFard = document.getElementById('fard');
+    var sound = epicFard.cloneNode();
+
+    sound.play();
+}
+
+function boomPlay() {
+    var epicBum = document.getElementById('boom');
+    var sound = epicBum.cloneNode();
+
+    sound.play();
+}
+
+function hintsRed() {
+    window.location.href = 'assets/hints.html';
+}
