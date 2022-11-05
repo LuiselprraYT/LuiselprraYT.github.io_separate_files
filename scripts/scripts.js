d819e23fe2a6b64304645d8b0c42024f29ec3478
Luis Enrique S <71048615+LuiselprraYT@users.noreply.github.com>
2022-11-05 12:56:37 -0700

    finally updated dis yay

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,8 +5,30 @@ function yes() {
         'what are you doing here?',
         'if you dont want to lose time just leave this page :)',
         'pretty hungry ngl',
-        'i want some cookies, you have?'
+        'i want some cookies, you have?',
+        'how many messages can fit within 1gb?',
+        'can i put ma balls in yo jaw?',
+        'this thing will never be finished'
     ];
-    var item = text[Math.floor(Math.random() * text.length)];
-    document.getElementById('randtext').innerHTML = item;
-}
+    var theChance = Math.floor(Math.random() * 1000);
+    if (theChance === 727) {
+        document.getElementById('randtext'). innerHTML = 'this message has 1/1000 or 0.1% to appear, fr';
+    } else {
+        var item = text[Math.floor(Math.random() * text.length)];
+        document.getElementById('randtext').innerHTML = item;
+    };
+};
+function yesV2() {
+    var textV2 = [
+        'loadin\'...',
+        'makin\' the website work...',
+        'finishing to do nothing...',
+        'bakin\' cookies...',
+        'setup has failed to install, try again',
+        'waiting for input...',
+        'press ctrl+alt+del to continue',
+        '<a href="https://youtu.be/dQw4w9WgXcQ">click here to load faster</a>'
+    ];
+    var itemV2 = textV2[Math.floor(Math.random() * textV2.length)];
+    document.getElementById('theLoad').innerHTML = itemV2;
+};
